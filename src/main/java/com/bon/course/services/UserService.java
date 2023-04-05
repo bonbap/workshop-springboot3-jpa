fcd5e0a92cfbd7d710185ed269ae558d722f449b
unknown <contatobonbap@gmail.com>
2023-04-05 16:23:26 -0300

    Service layer, component registration

---
+++ src/main/java/com/bon/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.bon.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bon.course.entities.User;
+import com.bon.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
